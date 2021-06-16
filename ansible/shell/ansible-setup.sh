717574e933d29a97ad01af1e50a886a9223ce44a
An Nguyen <ngphban@gmail.com>
2021-06-16 16:47:05 +0700

    Add Ansible playbooks + AWS

--- ansible/shell/ansible-setup.sh
+++ ansible/shell/ansible-setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# install ansible
 yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install ansible
 
-cd /home/vagrant/provision
-ansible-playbook playbooks/environment.yml
+# install boto3 module for Ansible
+yum -y install python-pip
+pip install boto boto3
+
+# Set less ssh key less permissive
+cd /home/vagrant/provision/aws
+chmod 600 myrdr66.pem
+
+# Run your playbooks
+# ansible-playbook playbooks/environment.yml
